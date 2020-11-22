5c374c63bbfb3a0f65c4f8e3523e74dff02da64c
Miguel Castillo <miguelcastillo1992lp@gmail.com>
2020-11-21 23:01:18 -0300

    Exclusion mutua para la escritura del resultado .csv

--- Paralelo/Master Worker/matmul-block-parallel.c
+++ Paralelo/Master Worker/matmul-block-parallel.c
@@ -6,7 +6,7 @@
  * + OpenMP, modelo Master Worker
  *
  * Para compilar:
- * mpicc -fopenmp -o <executable> matmul-block-parallel.c
+ * mpicc matmul-block-parallel.c -fopenmp -O3 -o matmul-block-parallel
  *
  * Para ejecutar:
  * mpirun -np <processes> --hostfile <hostfile> <executable> <sizeMatrix> <sizeBlock> <threads>
@@ -19,11 +19,17 @@
 #include <mpi.h>
 #include <omp.h>
 
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+
 #define USAGE_MSG "Use mpirun -np <processes> --hostfile <hostfile> %s <sizeMatrix> <sizeBlock> <threads>\n"
 #define BASENAME_MPI_CSV "result_matmul-block-mpi.csv"
 #define BASENAME_MPI_OPENMP_CSV "result_matmul-block-mpi+openmp.csv"
-#define HEADER_CSV "sizeMatrix,sizeBlock,threads,time\n"
+#define HEADER_CSV "processes,threads,sizeMatrix,sizeBlock,time\n"
 #define min(a, b) ((a)<(b) ? (a) : (b))
+#define SEM_NAME "/mysem"
 
 void imprimirMatrices(double *A, double *B, double * C, int N);
 void guardarEjecucion(int nproc, int sizeMatrix, int sizeBlock, int threads, double time);
@@ -109,7 +115,18 @@ int main(int argc, char **argv) {
     }
     printf("\nDuración total de la multiplicacion de matrices %4f segundos\n\n", t2 - t1);
     /* La ejecucion se almacena en un archivo .csv */
+
+    sem_t *sem;
+    sem = sem_open(SEM_NAME, O_CREAT|O_EXCL, S_IRUSR|S_IWUSR, 1);
+    if (sem ==  SEM_FAILED && errno == EEXIST) {
+      //printf("semaphore  appears  to  exist  already !\n");
+      sem = sem_open(SEM_NAME , 0);
+    }
+    sem_wait(sem);
     guardarEjecucion(nproc, sizeMatrix, sizeBlock, threads, t2 - t1);
+    sem_post(sem);
+    sem_close(sem);
+
     free(A);
     free(C);
   }
@@ -126,50 +143,49 @@ int main(int argc, char **argv) {
 void matmulblks(double *a, double *b, double *c, int n, int bs, int p)
 {
   int i0, j0, k0, i, j, k;
-  int dispA, dispB, dispC;
+  int disp, dispB;
   double temp;
   for (i0 = 0; i0 < n / p; i0 += bs) {
     for (j0 = 0; j0 < n; j0 += bs) {
       for (k0 = 0; k0 < n; k0 += bs) {
         for (i = i0; i < min(i0 + bs, n / p); i++) {
-          dispC = i * n;
-          dispA = i * n;
+          disp = i * n;
           for (j = j0; j < min(j0 + bs, n); j++) {
             temp = 0.0;
             dispB = j * n;
             for (k = k0; k < min(k0 + bs, n); k++) {
-              temp += a[dispA + k] * b[dispB + k];
+              temp += a[disp + k] * b[dispB + k];
             }
-            c[dispC + j] += temp;
+            c[disp + j] += temp;
           }
         }
       }
     }
   }
+
 }
 
 void matmulblksOpenMP(double *a, double *b, double *c, int n, int bs, int p, int t)
 {
   int i0, j0, k0, i, j, k;
-  int dispA, dispB, dispC;
+  int disp, dispB;
   double temp;
   omp_set_num_threads(t);
-  #pragma omp parallel shared(c,a,b,n,bs,p) private(i0,j0,k0,i,j,k,dispA,dispB,dispC,temp)
+  #pragma omp parallel shared(c,a,b,n,bs,p) private(i0,j0,k0,i,j,k,disp,dispB,temp)
   {
     #pragma omp for schedule(static)
     for (i0 = 0; i0 < n / p; i0 += bs) {
       for (j0 = 0; j0 < n; j0 += bs) {
         for (k0 = 0; k0 < n; k0 += bs) {
           for (i = i0; i < min(i0 + bs, n / p); i++) {
-            dispC = i * n;
-            dispA = i * n;
+            disp = i * n;
             for (j = j0; j < min(j0 + bs, n); j++) {
               temp = 0.0;
               dispB = j * n;
               for (k = k0; k < min(k0 + bs, n); k++) {
-                temp += a[dispA + k] * b[dispB + k];
+                temp += a[disp + k] * b[dispB + k];
               }
-              c[dispC + j] += temp;
+              c[disp + j] += temp;
             }
           }
         }
@@ -236,8 +252,8 @@ FILE* makeOutfile(char* basename, char *header) {
     outfile = fopen(basename, "a");
   } else {
     outfile = fopen(basename, "w");
-    //if (outfile != NULL)
-    //  fprintf(outfile, "%s", header);
+    if (outfile != NULL)
+    	fprintf(outfile, "%s", header);
   }
   if (outfile == NULL)
     fprintf(stderr, "Cannot open outfile %s\n", basename);
@@ -248,13 +264,13 @@ void guardarEjecucion(int nproc, int sizeMatrix, int sizeBlock, int threads, dou
   FILE* outfile;
   if(threads == 1) {
 	outfile = makeOutfile(BASENAME_MPI_CSV, HEADER_CSV);
-	fprintf(outfile, "\"(%d,%d,%d)\",%4f\n", nproc, sizeMatrix, sizeBlock, time);
   } else {
 	outfile = makeOutfile(BASENAME_MPI_OPENMP_CSV, HEADER_CSV);
-	fprintf(outfile, "\"(%d,%d,%d,%d)\",%4f\n", nproc, sizeMatrix, sizeBlock, threads, time);
   }
-  //fprintf(outfile, "%d,%d,%d,%4f\n", sizeMatrix, sizeBlock, threads, time);
-  fclose(outfile);
+  if(outfile != NULL){
+	fprintf(outfile, "%d,%d,%d,%d,%4f\n", nproc, threads > 1 ? nproc * threads : threads, sizeMatrix, sizeBlock, time);
+	fclose(outfile);
+  }
 }
 
 long get_integer_arg(int argc, char* argv[], int arg_index,
